a43ad5042f5019367fa2630820aba85cc968d2cf
sooh329 <glay@kakao.com>
2024-12-09 19:53:03 +0900

    convertApi 토큰 값 변경

--- src/main/java/Convert.java
+++ src/main/java/Convert.java
@@ -41,7 +41,7 @@ public class Convert extends HttpServlet {
         }
         
         // ConvertAPI 설정
-        Config.setDefaultApiCredentials("secret_pktgaRpZo2tCm7xe");
+        Config.setDefaultApiCredentials("secret_9lf6WApGNeaqr5MK");
 
         // 업로드된 파일을 가져오기
         Part filePart = request.getPart("file");
